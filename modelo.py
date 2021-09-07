7bbaa5b522c6d18cd9b9c3694ae91896f97b7632
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-09-07 08:57:19 -0300

    atributos e metodos

--- modelo.py
+++ modelo.py
@@ -1,17 +1,27 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
+
+
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 
 atlanta = Serie('atlanta', 2018, 2)
-
-print(f'Nome {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas}')
+atlanta.dar_like()
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
