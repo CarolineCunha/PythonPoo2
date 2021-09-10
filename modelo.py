b687a3611d1cf7c1b6f313f1e2afae3f00506ed7
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-09-10 07:17:45 -0300

    representação textual de objetos

--- modelo.py
+++ modelo.py
@@ -19,24 +19,24 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self._likes}')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes}'
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self.duracao} - {self._likes}')
+    def __str__(self):
+        return (f'{self._nome} - {self.ano} - {self.duracao} - {self._likes}')
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self.temporadas} - {self._likes}')
+    def __str__(self):
+        return (f'{self._nome} - {self.ano} - {self.temporadas} - {self._likes}')
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -47,4 +47,4 @@ atlanta.dar_like()
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series:
-    programa.imprime()
+    print(programa)
